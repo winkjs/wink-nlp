513b75c43991d9cdf1abaca4ab5f28b484668f8e
gpi <gpimpale29@gmail.com>
2021-07-29 23:55:09 -0700

    better handling of types

--- types/test.ts
+++ types/test.ts
@@ -1,14 +1,43 @@
-import winkNlp from 'wink-nlp';
+import winkNlp, {
+    Sentences,
+    CustomEntities,
+    SelectedCustomEntities,
+    Entities,
+    SelectedEntities,
+    Tokens,
+    SelectedTokens,
+    ItsFunction,
+    AsFunction,
+} from 'wink-nlp';
 import model from 'wink-eng-lite-web-model';
 
 // $ExpectType WinkMethods
 winkNlp(model);
 
 // $ExpectType WinkMethods
-winkNlp(model, ["foo"]);
+const nlp = winkNlp(model, ["foo"]);
 
 // $ExpectType ItsHelpers
-winkNlp(model).its;
+const its = nlp.its;
 
 // $ExpectType AsHelpers
-winkNlp(model).as;
+const as = winkNlp(model).as;
+
+// $ExpectType Document
+const doc = nlp.readDoc('test');
+
+// $ExpectType CustomEntities
+const customEntities = doc.customEntities();
+
+// $ExpectType string[]
+customEntities.out(its.value, as.array);
+
+// $ExpectType string[]
+customEntities.out(its.value, as.array);
+
+type OutApplicable = Sentences | CustomEntities | SelectedCustomEntities | Entities | SelectedEntities | Tokens | SelectedTokens;
+
+// $ExpectType <T, U>(toOut: OutApplicable, itsf: ItsFunction<T>, asf: AsFunction<T, U>) => U
+function myOut<T, U>(toOut: OutApplicable, itsf: ItsFunction<T>, asf: AsFunction<T, U>): U {
+  return (toOut.out(itsf, asf) as any) as U;
+}
