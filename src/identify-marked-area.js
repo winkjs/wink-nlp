433e5793887569222c76e5a7b6e33729c43d7f99
Sanjaya Kumar Saxena <sanjaya@graype.in>
2022-12-06 19:57:27 +0530

    docs(identify-marked-area): add header coomments

--- src/identify-marked-area.js
+++ src/identify-marked-area.js
@@ -1,3 +1,47 @@
+//     wink-nlp
+//
+//     Copyright (C) GRAYPE Systems Private Limited
+//
+//     This file is part of “wink-nlp”.
+//
+//     Permission is hereby granted, free of charge, to any
+//     person obtaining a copy of this software and
+//     associated documentation files (the "Software"), to
+//     deal in the Software without restriction, including
+//     without limitation the rights to use, copy, modify,
+//     merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to
+//     whom the Software is furnished to do so, subject to
+//     the following conditions:
+//
+//     The above copyright notice and this permission notice
+//     shall be included in all copies or substantial
+//     portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//     ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//     TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//     PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//     THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+//     CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//     CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//     DEALINGS IN THE SOFTWARE.
+
+//
+
+// ## identifyMarkedArea
+/**
+ *
+ * Identifies the area to be marked within the detected entitity's span, which is
+ * extracted as the value.
+ *
+ * @param {number[]} mark contains the target area to be extracted from within
+ *                        the entitity's span, defined as `[ firstIndex, lastIndex ]`.
+ * @param {number} length of the entity's span.
+ * @return {number[]}     array containing the modifiers for the orginal span.
+ * @private
+*/
 const identifyMarkedArea = function ( mark, length ) {
   // Length Minus 1.
   const lm1 = length - 1;
